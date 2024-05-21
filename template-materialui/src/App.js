a4e581f9b56c3b535c39679af12155e98381a93b
gitluiztorres <luizg.t@outlook.com>
2024-05-20 21:03:16 -0300

    Commit para atividade pratica

---
+++ template-materialui/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/LOGIN";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
